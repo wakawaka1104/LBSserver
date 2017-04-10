511f370fec51184b661118ab2a4f6f58a6f9fc3e
Shuntaro Takahara <wakasha444@gmail.com>
2017-04-11 02:14:01 +0900

    first commit

---
+++ LBSserver/src/server/MainApplication.java
@@ -0,0 +1,23 @@
+package server;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		InetAddress addr;
+		try {
+			addr = InetAddress.getLocalHost();
+			TestServer ts = new TestServer(addr, 11111);
+			Thread serverThread = new Thread(ts);
+			serverThread.start();
+			
+		} catch (UnknownHostException e1) {
+			System.err.println("aaa");
+			e1.printStackTrace();
+		}
+
+	}
+
+}
