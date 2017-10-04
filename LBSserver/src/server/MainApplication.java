ff1d44843fda6612e83c7f7f9d2eec070ee59580
takahara <takahara@mtl.t.u-tokyo.ac.jp>
2017-10-04 18:33:48 +0900

    added system that sends SlaveList cyclically

--- LBSserver/src/server/MainApplication.java
+++ LBSserver/src/server/MainApplication.java
@@ -1,9 +1,14 @@
 package server;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import asset.SlaveList;
 import tcpIp.SocketServer;
 
 public class MainApplication {
+
+	public static SocketServer ts;
 
 	public static void main(String[] args) {
 		String addr;
@@ -11,16 +16,24 @@ public class MainApplication {
 			SlaveList.loadList();
 			System.out.println(SlaveList.getInstance().toString());
 			addr = "localhost";
-			SocketServer ts = new SocketServer(addr, 11111);
+			ts = new SocketServer(addr, 11111);
 			Thread serverThread = new Thread(ts);
 			serverThread.start();
 
+			Timer timer = new Timer();
+			timer.schedule(new ListUpdater(), 0, 100000);
 
-		} catch (Exception e1) {
+		}catch (Exception e1) {
 			System.err.println("aaa");
 			e1.printStackTrace();
 		}
 
 	}
+}
 
+class ListUpdater extends TimerTask{
+	@Override
+	public void run() {
+		MainApplication.ts.asyncSend(SlaveList.getInstance(), (byte)0);
+	}
 }
