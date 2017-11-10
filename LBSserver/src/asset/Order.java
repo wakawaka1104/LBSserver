2ecc4cb285a635497b900edbb41dfdeb969bedd3
takahara <takahara@mtl.t.u-tokyo.ac.jp>
2017-11-10 13:16:40 +0900

    Member added in Order Class

--- LBSserver/src/asset/Order.java
+++ LBSserver/src/asset/Order.java
@@ -8,6 +8,7 @@ public class Order implements Serializable, Classifier {
 
 	private static final long serialVersionUID = 1L;
 	private String order = "";
+	private ByteFile file;
 
 	public Order(String order){
 		this.order = order;
@@ -23,6 +24,10 @@ public class Order implements Serializable, Classifier {
 
 	public void setMessage(String order){
 		this.order = order;
+	}
+
+	public void setFile(ByteFile file){
+		this.file = file;
 	}
 	@Override
 	public String getClassName() {
